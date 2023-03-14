84b8ff3ecc352316be5e21f8787821218cd9216f
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-03-14 15:43:49 +0500

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import { engine, create } from "express-handlebars"
+import mongoose from "mongoose";
+import flash from "connect-flash"
+import session from "express-session"
+import * as dotenv from "dotenv";
 import AuthRoutes from "./routes/auth.js"
 import ProductsRoutes from "./routes/products.js"
+
+
+dotenv.config()
+
+
 const app = express()
 const hbs = create({
     defaultLayout: 'main',
@@ -18,12 +27,31 @@ app.set('views', './views');
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(session({ secret: "Donni", resave: false, saveUninitialized: false }))
+app.use(flash())
+
+
 
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-const PORT = process.env.PORT ?? 5000
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+
+const startApp = () => {
+    try {
+        mongoose.set('strictQuery', true)
+        mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, })
+        const PORT = process.env.PORT ?? 5000
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startApp()
+
+
+// mongodb+srv://ddonierov96:<password>@doni.odgzc3z.mongodb.net/?retryWrites=true&w=majority
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
 const routes = Router()
 
 routes.get('/register', (req, res) => {
     res.render('register', {
         title: 'Register',
         isRegister: true,
+        loginError: req.flash('loginError'),
     })
 })
 
@@ -13,15 +16,44 @@ routes.get('/login', (req, res) => {
     res.render('login', {
         title: 'Login',
         isLogin: true,
+        registerError: "Error"
     })
 })
 
-routes.post('/login', (req, res) => {
-    console.log(req.body);
+
+
+routes.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        req.flash("loginError", "All fields is required")
+        res.redirect('/login')
+    }
+
+    const existUser = await User.findOne({ email })
+    if (!existUser) {
+        console.log("User not found")
+        return;
+    }
+
+    const isPasswordEqual = await bcrypt.compare(password, existUser.password)
+    if (!isPasswordEqual) {
+        console.log("Password wrong")
+        return;
+    }
+
     res.redirect("/")
 })
-routes.post('/register', (req, res) => {
-    console.log(req.body);
+routes.post('/register', async (req, res) => {
+    const hashPassword = await bcrypt.hash(req.body.password, 10)
+    const userData = {
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        email: req.body.email,
+        password: hashPassword,
+    }
+    const user = await User.create(userData)
+    console.log(user);
     res.redirect("/")
 })
 
